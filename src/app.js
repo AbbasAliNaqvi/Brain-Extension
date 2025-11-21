20f139d0aba3770b39a59822353e3ef37f5f6865
AbbasAliNaqvi <abbasalinaqvi888@gmail.com>
2025-11-21 15:58:34 +0530

    Set up core backend infrastructure: Dockerized Express server, DB connection, env config, middleware setup, and scalable project structure.

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev")); 
+
+app.get("/", (req, res) => {
+  res.send("Brain Extension is Running Perfectly!");
+});
+
+module.exports = app;
