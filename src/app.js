d4daf8a19ace869194a6c22f2cb294870f95c698
AbbasAliNaqvi <abbasalinaqvi888@gmail.com>
2026-01-26 18:30:00 +0530

    Implemented NoSQL injection and XSS data sanitization

--- src/app.js
+++ src/app.js
@@ -5,6 +5,9 @@ const helmet = require("helmet");
 const compression = require("compression");
 const swaggerUi = require('swagger-ui-express');
 
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+
 require("dotenv").config();
 
 const brainShield = require("./middleware/brainShield");
@@ -21,8 +24,11 @@ app.use(compression());
 app.use(rateLimiter);
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10kb' }));
 app.use(morgan("dev"));
+
+app.use(mongoSanitize());
+app.use(xss());
 
 app.get("/health", (req, res) => {
     res.status(200).json({
